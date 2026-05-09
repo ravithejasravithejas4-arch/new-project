16bcdc5bd160d4329488edc414341f7fcbe73e0f
ravithejasravithejas4-arch <ravithejasravithejas4@gmail.com>
2026-05-09 15:14:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Audio Video Utility Studio",
+    layout="wide"
+)
+
+st.title("🎬 Audio Video Utility Studio")
+
+st.write("Welcome to the Media Processing Project")
